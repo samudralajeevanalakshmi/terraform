4b7a7c39099c9048dc5ef8ed51c17cfdfa345b60
Samudrala Jeevana Lakshmi <113153018+samudralajeevanalakshmi@users.noreply.github.com>
2024-06-05 12:23:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,6 +33,16 @@ variable "static_web_app_name" {
   default     = "mywebappteam4"
 }
 
+variable "acr_name" {
+  description = "Name of the Azure Container Registry (ACR)"
+  default     = "myacrteam4"
+}
+
+variable "aks_cluster_name" {
+  description = "Name of the Azure Kubernetes Service (AKS) cluster"
+  default     = "myaksteam4"
+}
+
 # Resource Group
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
@@ -46,10 +56,37 @@ resource "azurerm_mysql_flexible_server" "mysql_flexible_server" {
   resource_group_name = azurerm_resource_group.main.name
   administrator_login = var.mysql_admin_username
   administrator_password = var.mysql_admin_password
-  sku_name            = "MYSQL_GP_Gen5_2"
+  sku_name            = "GP_Gen5_2"  # Corrected SKU name
 
   tags = {
     environment = "production"
+  }
+}
+
+# Azure Container Registry (ACR)
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
+  sku                 = "Standard"
+  admin_enabled       = true
+}
+
+# Azure Kubernetes Service (AKS)
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = var.aks_cluster_name
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
   }
 }
 
