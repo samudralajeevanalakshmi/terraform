ebfd66d9209f8d2a6c5ec2c7f658e444d3fc3092
Samudrala Jeevana Lakshmi <113153018+samudralajeevanalakshmi@users.noreply.github.com>
2024-06-05 11:56:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,14 +41,13 @@ resource "azurerm_mysql_flexible_server" "mysql_flexible_server" {
   location            = var.location
   resource_group_name = azurerm_resource_group.main.name
   administrator_login = var.mysql_admin_username
-  admin_password      = var.mysql_admin_password
   sku_name            = "Standard_D2s_v3"
-  charset             = "utf8mb4"
 
   storage {
     storage_size_gb = 20
   }
 }
+
 
 # Static Web App
 resource "azurerm_static_site" "static_web_app" {
